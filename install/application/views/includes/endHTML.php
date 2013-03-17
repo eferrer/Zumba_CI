77809bf132074e55349ead5f773f168fa9892b16
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-03-18 10:48:22 +1300

    Created footer file

--- install/application/views/includes/endHTML.php
+++ install/application/views/includes/endHTML.php
@@ -1,14 +1,58 @@
-<footer>&copy; 2013. Eileen Ferrer</footer>
+<footer><!--footer begins-->
+      <section id="footerLinks"><!--social and contact links begins-->
+        <section class="zin"><!--zin network image begins-->
+          <article class="zinNetwork">
+            <!-- <img src="assets/images/full/photo3.jpg"/> -->
+          </article>
+          <p class="zinDate">since June 2011</p>
+        </section><!--zin network image ends-->
+        
+        <section class="atomic"><!--zumbatomic image begins-->
+          <article class="zumbatomic">
+            <!-- <img src="assets/images/full/photo3.jpg"/> -->
+          </article>
+        </section><!--zumbatomic image ends-->
 
-<!--========================JAVASCRIPT========================-->
+        
+        <article class="social">
+        <ul >
+          <li class="connect"><a  href="connect.html"><span class="fontAwesome">&#xf082;</span>&nbsp;&nbsp;<span class="text">Connect with Us</span></a></li>
+          <li class="contactUs"><a href="contact.html"><span class="fontAwesome">&#xf0e0;</span>&nbsp;&nbsp;<span class="text">Contact Me</span></a></li>
+        </ul>
+        </article>
+          
+      </section><!--social and contact links ends-->
+    
+     <section id="copyright"> <!-- bottom footer section begins   -->
+      <p>Site by Eileen Ferrer. &copy;2013.</p>
+    </section><!--bottom footer section ends-->
+  </footer><!--footer ends-->
 
-<script>window.jQuery || document.write('<script src="http://webconsultant.co.nz/jslibs/jquery-1.8.3.min.js"><\/script>')</script><!--make this your website address where you have the jquery library stored-->
 
-<!-- Javascripts for form validation -->
-<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
-<script>window.jQuery || document.write('<script src="http://natcoll.net.nz/JS-LIBS/jquery-validation.1.9.js"><\/script>')</script>
 
-<script type="text/javascript" src="<?php echo base_url();?>js/main.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>js/mojo.js"></script>
+<!-- Grab Google CDN's jQuery. fall back to another if necessary-->
+<!--http://encosia.com/3-reasons-why-you-should-let-google-host-jquery-for-you/ -->  
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="http://webconsultant.co.nz/jslibs/jquery-1.8.3.min.js"><\/script>')</script>
+
+<!-- <script src="javascripts/responsiveslides.js"></script>
+<script src="javascripts/responsiveslides.min.js"></script> -->
+
+<script src="<?=base_url()?>/js/main.js"></script> 
+
+<script src="<?=base_url()?>/js/jquery.easing.1.3.js" type="text/javascript"></script>  
+<script src="<?=base_url()?>/js/animated-menu.js" type="text/javascript"></script>
+
+<script>
+    // This is embedded for DEMO SCRIPT ONLY. 
+     // make sure the document is ready before starting to do anything.  Place all your jquery calls inside this function
+        $(document).ready(function(){
+            // Your code goes here
+        
+        });  // put this closing bracket combo right beside the closing script tag.    
+</script>  
 </body>
 </html>
+
+
+
