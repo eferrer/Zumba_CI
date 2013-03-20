6b8d694694a948ab4bbede8dd28e0d465e7199c1
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-03-20 18:23:07 +1300

    Test 1 Controller calling model correctly

--- install/application/controllers/about.php
+++ install/application/controllers/about.php
@@ -11,22 +11,56 @@ class About extends CI_Controller {
     // {
     //         $data=array();
     //         $data['content'] = "this is the about view";
+    //
     //         $this->load->view('includes/startHTML');
     //         $this->load->view('aboutView', $data);
     //         $this->load->view('includes/endHTML');
     // }
     
     //==============================================================
+    // Test 1. Test that the about controller is calling Cmsmodel correctly
+    // WORKS!!
+    
     public function index()
     {
             $data=array();
-        // if($this->uri->segment(3) == '1'){
+
             $this->load->model('Cmsmodel');
-            //$data['content'] = $this->Cmsmodel->getContentByPageID();
-            $data['content'] = "this is not the about view";
+            $data['content'] = $this->Cmsmodel->getContentByPageID();
+            
             $this->load->view('includes/startHTML');
             $this->load->view('aboutView', $data);
             $this->load->view('includes/endHTML');
+    }
+    
+     //==============================================================
+    
+    // Test 2. Test with the page ID hardcoded
+    
+    
+    public function index()
+    {
+            $data=array();
+
+            $this->load->model('Cmsmodel');
+            $data['content'] = $this->Cmsmodel->getContentByPageID();
+            
+            $this->load->view('includes/startHTML');
+            $this->load->view('aboutView', $data);
+            $this->load->view('includes/endHTML');
+    }
+    
+    
+    //  public function index()
+    // {
+    //         $data=array();
+    //     // if($this->uri->segment(3) == '1'){
+    //         $this->load->model('Cmsmodel');
+    //         $data['content'] = $this->Cmsmodel->getContentByPageID();
+            
+    //         $this->load->view('includes/startHTML');
+    //         $this->load->view('aboutView', $data);
+    //         $this->load->view('includes/endHTML');
         // }
         // else{
         //     $data['content'] = 'hardcoded';
--- install/application/models/cmsmodel.php
+++ install/application/models/cmsmodel.php
@@ -1,15 +1,34 @@
 <?php
 
-class About extends CI_Model{
+class Cmsmodel extends CI_Model{
     
     function __construct()
     {
         parent::__construct();
     }
     
+    //==============================================================
+    // Test 1. Test that the about controller is calling Cmsmodel correctly
+    // WORKS!!
+    
+     public function getContentByPageID()
+    {
+        return "This is something written in the cmsmodel";
+    }
+    
+    //==============================================================
+    
+    
     public function getContentByPageID()
     {
+        // $id = ($this->uri->segment(3))?$this->uri->segment(3):1;
+        // $arr = array('pageID'=>$this->uri->segment(3));
         
+        $sql = "SELECT contentDetails
+                FROM tbContent
+                WHERE pageID=2";
+                
+        return $this->db->query($sql);
     }
     
 } // end of class About
