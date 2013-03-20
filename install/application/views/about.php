1aaf323916d68cdcef7f0af826ad276b65f9a2fe
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-03-20 14:00:25 +1300

    Corrected paragraphs

--- install/application/views/about.php
+++ install/application/views/about.php
@@ -9,7 +9,7 @@
           <p>I'm a busy mother of 4 children and originally from the USA. I would love to inspire many other women and mothers to give Zumba Fitness a go!</p>
           <p> Many women have said to me, I would love to give it a try, but I'm too afraid, or I don't how to dance. Here's your chance to give it a go!</P>
           <p> My class will be perfect for those beginners, but it will also be a great workout for those that have also done it before.</p>
-          <p>I have taught from ages 5-75 years old! So no matter what your age, shape or fitness/dance level is, become inspired with me to "Party Your Shape Into Finess"!
+          <p>I have taught from ages 5-75 years old! So no matter what your age, shape or fitness/dance level is, become inspired with me to "Party Your Shape Into Fitness"!
           <P> If I can do it, so can you!</p>
         </article><!--content paragraphs end-->
         
