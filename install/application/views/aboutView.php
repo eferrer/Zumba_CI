106da38f1a2b2d3f72d3eb48373f198e58fa8951
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-03-22 09:04:56 +1300

    Retrieving About Me content completed

--- install/application/views/aboutView.php
+++ install/application/views/aboutView.php
@@ -17,11 +17,6 @@
             $oContent = $content->row();
             echo nl2br($oContent->contentDetails);
             
-        
-      
-
-            
-            
             
             ?>
             
