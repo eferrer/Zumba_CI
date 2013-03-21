49ea40d726369f6aba02c8f3d9a838e96735f5a3
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-03-22 10:24:56 +1300

    Classes page content

--- install/application/controllers/classtimes.php
+++ install/application/controllers/classtimes.php
@@ -2,27 +2,73 @@
 
 class Classtimes extends CI_Controller {
 
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     *      http://example.com/index.php/welcome
-     *  - or -  
-     *      http://example.com/index.php/welcome/index
-     *  - or -
-     * Since this controller is set as the default controller in 
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see http://codeigniter.com/user_guide/general/urls.html
-     */
+    //==============================================================
+    // Hardcode something for the content 
+    // Display that something out in the about view
+    // WORKS!!    
+
+    // public function index()
+    // {
+    //         $data=array();
+    //         $data['content'] = "this is the about view";
+    //
+    //         $this->load->view('includes/startHTML');
+    //         $this->load->view('aboutView', $data);
+    //         $this->load->view('includes/endHTML');
+    // }
+    
+    //==============================================================
+    // Test 1. Test that the about controller is calling Cmsmodel correctly
+    // WORKS!!
+    
+    // public function index()
+    // {
+    //         $data=array();
+
+    //         $this->load->model('Cmsmodel');
+    //         $data['content'] = $this->Cmsmodel->getContentByPageID();
+            
+    //         $this->load->view('includes/startHTML');
+    //         $this->load->view('aboutView', $data);
+    //         $this->load->view('includes/endHTML');
+    // }
+    
+    //==============================================================
+    // Test 2. Test with the page ID hardcoded
+    
+
     public function index()
     {
-        $this->load->view('includes/startHTML');
-        $this->load->view('classes');
-        $this->load->view('includes/endHTML');
+            $data=array();
+
+            $this->load->model('Cmsmodel');
+            $data['content'] = $this->Cmsmodel->getContentByPageIDc();
+            
+            $this->load->view('includes/startHTML');
+            $this->load->view('classesView', $data);
+            $this->load->view('includes/endHTML');
     }
+    
+    
+    //  public function index()
+    // {
+    //         $data=array();
+    //     // if($this->uri->segment(3) == '1'){
+    //         $this->load->model('Cmsmodel');
+    //         $data['content'] = $this->Cmsmodel->getContentByPageID();
+            
+    //         $this->load->view('includes/startHTML');
+    //         $this->load->view('aboutView', $data);
+    //         $this->load->view('includes/endHTML');
+        // }
+        // else{
+        //     $data['content'] = 'hardcoded';
+
+        //     $this->load->view('includes/startHTML');
+        //     $this->load->view("alternateView", $data);
+        //     $this->load->view("includes/endHTML");
+        // }
+    //}
 
 }
 
--- install/application/views/classesView.php
+++ install/application/views/classesView.php
@@ -1,5 +1,29 @@
     
     <section id="mainContainer" class="cf"><!--class schedule container begins-->
+
+
+      <?php 
+            // $oContent = $content->result();
+
+            // echo "<pre>";
+            // print_r($oContent);
+            // echo "</pre>";
+
+
+            $aContent = $content->result_array();
+
+            print_r($aContent[1]);
+            echo $aContent[1]['contentDetails'];
+
+            echo "<pre>";
+            print_r($aContent);
+            echo "</pre>";
+
+            // $oContent = $content->row();
+            // echo nl2br($oContent->contentDetails);
+            
+            ?>
+
     
           <h1 class="classHeading">CLASS SCHEDULE</h1>
 
@@ -48,10 +72,40 @@
             <section id="priceSchedule"> <!--price schedule content begins-->    
               
               <article class="classPrice">
-                <h3>CASUAL CLASS:</h3><p>$10 per class</p>
-                <h3>CONCESSION CARDS:</h3><p>$40 for 5 classes<br/>$75 for 10 classes</p>
+                <h3>CASUAL CLASS:</h3>
+                <p>
+                  <!-- $10 per class -->
+
+                  <?php
+
+                  $aContent = $content->result_array();
+                  echo $aContent[1]['contentDetails'];
+
+                  ?>
+
+                </p>
+                <h3>CONCESSION CARDS:</h3>
+                <p>
+                  <!-- $40 for 5 classes<br/>$75 for 10 classes -->
+
+                  <?php
+
+                  $aContent = $content->result_array();
+                  echo nl2br($aContent[2]['contentDetails']);
+
+                  ?>
+                </p>
               
-                <p>Cards can be used only for the Tuesday, Wednesday, and Friday classes.<br/>There is no expiry date on concession cards. </p>
+                <p>
+                  <!-- Cards can be used only for the Tuesday, Wednesday, and Friday classes.<br/>There is no expiry date on concession cards.  -->
+
+                  <?php
+
+                  $aContent = $content->result_array();
+                  echo nl2br($aContent[3]['contentDetails']);
+
+                  ?>
+                </p>
               </article>
 
             </section> <!--price schedule content ends--> 
@@ -71,11 +125,19 @@
 
           <h3>WHAT TO EXPECT</h3>
           <article class="studentInfo"><!--content paragraphs begin-->
-            <p>Most Zumba classes are an hour long. They begin with a dynamic warm-up and end with a cool down and some static stretching. The workout portion is broken down by song, with alternating fast and slow routines. As a result, you’ll burn calories – up to 800 in 1 class- as you tone and sculpt your body.. While many of the moves the instructors use are pre-choreographed by Zumba, instructors often add their own flair, dance steps or even create an entirely new routine to their favorite songs. Common moves in class are mambas, V-steps with exaggerated hips, cha-cha-chas and side touches.</p>
+            <!-- <p>Most Zumba classes are an hour long. They begin with a dynamic warm-up and end with a cool down and some static stretching. The workout portion is broken down by song, with alternating fast and slow routines. As a result, you’ll burn calories – up to 800 in 1 class- as you tone and sculpt your body.. While many of the moves the instructors use are pre-choreographed by Zumba, instructors often add their own flair, dance steps or even create an entirely new routine to their favorite songs. Common moves in class are mambas, V-steps with exaggerated hips, cha-cha-chas and side touches.</p>
 
             <p>Unlike other aerobics classes, where you learn a move and then add on more moves to create a routine, Zumba uses different parts of the song, such as the chorus or a particular phrase or sound in the song, as the basis of its choreography. Zumba is also unique in that its instructors don't speak much. Instead they cue you on what to do with hand signals of which direction to move, or a loud hand clap that signifies that it's time to move to a new move. Overall, participants learn as they go through repetition and by example. This can be frustrating for first-timers, but because Zumba uses a lot of the same songs over and over, you will get the hang of it after a few Zumba classes.</p>
           
-            <p>Zumba is one of the highest energy workouts, but unlike many other exercise routines, zumba really doesn't feel like you're working out at all—you just get caught up in the fun of the music and the moves. Before you know it, a whole heart-pumping hour has passed! </p>
+            <p>Zumba is one of the highest energy workouts, but unlike many other exercise routines, zumba really doesn't feel like you're working out at all—you just get caught up in the fun of the music and the moves. Before you know it, a whole heart-pumping hour has passed! </p> -->
+            <p>
+            <?php
+
+                  $aContent = $content->result_array();
+                  echo nl2br($aContent[4]['contentDetails']);
+
+                  ?>
+              </p>
           </article><!--content paragraphs end-->
           
           <h3>WHAT YOU WILL NEED</h3>
